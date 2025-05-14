6914598cc391f20c4fb128f69f0f8fc607ac44fe
identitynulll <identitynulll@gmail.com>
2025-05-14 23:05:28 +0500

    Compeleted sign up

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ document.addEventListener("mousemove", (event) => {
         element.style.transform = `translate(${x * speed * 20}px, ${y * speed * 20}px)`
     }) 
 })
+
+
+const signinButton = document.getElementById('signinButton')
+const signinPage = document.getElementById('signinPage')
+const closeIcon = document.getElementById("closeIcon")
+
+signinButton.addEventListener("click", function () {
+    signinPage.classList.remove("closeSignin")
+    signinPage.classList.add("openSignin")
+})
+
+closeIcon.addEventListener("click", function() {
+    signinPage.classList.remove("openSignin")
+    signinPage.classList.add("closeSignin")
+})
